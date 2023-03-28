5ff0a87625e12a396caaba49a44784cda0712828
Alberto Santinon <128792527+albersantinon@users.noreply.github.com>
2023-03-28 08:30:28 +0200

    Update task_02.py

--- task_02.py
+++ task_02.py
@@ -3,7 +3,8 @@ import json
 
 # read the json and put it into a dataframe
 path = 'C:/Users/a/PycharmProjects/dev/assignment/data/orders.jsonl'
-df = pd.read_json(path, lines=True)
+with open(path) as f:
+    df = pd.read_json(path, lines=True)
 
 # using json_normalize method to flatten the data
 json_structure = json.loads(df.to_json(orient="records"))
@@ -39,31 +40,28 @@ renamed_columns = {'ShippingAddress.StateOrRegion': 'StateOrRegion',
 
 
 def main():
-    try:
-        # transformations: nulls, datatypes and columns management
-        df_flat['IsBusinessOrder'].fillna(0, inplace=True)
-        df_flat['IsBusinessOrder'] = df_flat['IsBusinessOrder'].astype(int).astype(bool)
-        drop_columns(['ShippingAddress', 'OrderTotal', 'ShippingAddress.County'], df_flat)
-        lower_case('ShippingAddress.City', df_flat)
-        lower_case('ShippingAddress.StateOrRegion', df_flat)
-        df_renamed = rename_columns(renamed_columns, df_flat)
+    # transformations: nulls, datatypes and columns management
+    df_flat['IsBusinessOrder'].fillna(0, inplace=True)
+    df_flat['IsBusinessOrder'] = df_flat['IsBusinessOrder'].astype(int).astype(bool)
+    drop_columns(['ShippingAddress', 'OrderTotal', 'ShippingAddress.County'], df_flat)
+    lower_case('ShippingAddress.City', df_flat)
+    lower_case('ShippingAddress.StateOrRegion', df_flat)
+    df_renamed = rename_columns(renamed_columns, df_flat)
 
-        # removing duplicates
-        df_final = df_renamed.drop_duplicates()
+    # removing duplicates
+    df_final = df_renamed.drop_duplicates()
 
-        # new calculated columns
-        df_final['Year'] = pd.to_datetime(df_final['LastUpdateDate']).dt.strftime('%Y')
-        df_final['Year-Month'] = df_final['Year'] + pd.to_datetime(df_final['LastUpdateDate']).dt.strftime('%m')
-        df_final['Year-Quarter'] = pd.PeriodIndex(df_final['LastUpdateDate'], freq='Q')
-        df_final['Week'] = pd.to_datetime(df_final['LastUpdateDate']).dt.strftime('%Y') + 'W' + pd.to_datetime(
-            df_final['LastUpdateDate']).dt.strftime('%W')
-        df_final['Weekday'] = pd.to_datetime(df_final['LastUpdateDate']).dt.weekday + 1
+    # new calculated columns
+    df_final['Year'] = pd.to_datetime(df_final['LastUpdateDate']).dt.strftime('%Y')
+    df_final['Year-Month'] = df_final['Year'] + pd.to_datetime(df_final['LastUpdateDate']).dt.strftime('%m')
+    df_final['Year-Quarter'] = pd.PeriodIndex(df_final['LastUpdateDate'], freq='Q')
+    df_final['Week'] = pd.to_datetime(df_final['LastUpdateDate']).dt.strftime('%Y') + 'W' + pd.to_datetime(
+        df_final['LastUpdateDate']).dt.strftime('%W')
+    df_final['Weekday'] = pd.to_datetime(df_final['LastUpdateDate']).dt.weekday + 1
 
-        # exporting the dataframe to a csv
-        df_final.to_csv('file.csv', index=False)
-
-    finally:
-        print(df_final.to_string())
+    # exporting the dataframe to a csv
+    df_final.to_csv('file.csv', index=False)
+    print(df_final.to_string())
 
 
 main()
